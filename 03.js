65e6f3e784dd79afbfc42e4c8d1456b462051b32
Fadhel Muhammad <56714457+fdhlmhd@users.noreply.github.com>
2020-03-03 16:31:08 +0700

    update 03.js

--- 03.js
+++ 03.js
@@ -1,14 +1,13 @@
 
-new Promise((resolve, reject) => {
-    const hitung = [];
+new Promise((resolve) => {
+    
     for(let i = 1; i <=3; i++) {
         console.log(i)
         setTimeout(() => {
-            resolve(hitung)
+            resolve()
         }, 1000)
     }
 })
 
 .then(x => console.log('Done')
-).catch(error => console.log(error)
 )
